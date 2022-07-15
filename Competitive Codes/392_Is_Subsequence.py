769f432a38db47482cf5d7e010d336e293fdcffb
Surya-827 <vsuryakiran827@gmail.com>
2022-07-15 21:11:24 +0530

    Uploaded Solution for Is_Subsequence

---
+++ Competitive Codes/392_Is_Subsequence.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        if len(s) > len(t):return False
+        if len(s) == 0:return True
+        subsequence=0
+        for i in range(0,len(t)):
+            if subsequence <= len(s) -1:
+                print(s[subsequence])
+                if s[subsequence]==t[i]:
+
+                    subsequence+=1
+        return  subsequence == len(s)
+
+
+if __name__=="__main__":
+
+    obj = Solution()
+    s = input()
+    t = input()
+    print(obj.isSubsequence(s,t))
