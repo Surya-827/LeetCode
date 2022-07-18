25819253e81f950b795aded0ee15ad9b9671200b
Surya-827 <vsuryakiran827@gmail.com>
2022-07-18 12:50:04 +0530

    Uploaded Solution for Search matrix II

---
+++ Competitive Codes/240_Search_Matrix_2D_II.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                if matrix[i][j] == target:
+                    return True
+        return False
