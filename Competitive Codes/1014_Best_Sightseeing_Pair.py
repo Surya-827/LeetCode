4db8eca8b1cf1babc21bf21b794ebea4c96db73b
Surya-827 <vsuryakiran827@gmail.com>
2022-07-23 21:28:36 +0530

    Uploaded Solution for Best Time to Buy

--- Competitive Codes/1014_Best_Sightseeing_Pair.py
+++ Competitive Codes/1014_Best_Sightseeing_Pair.py
@@ -6,4 +6,4 @@ class Solution:
         for i in range(1, len(values)):
             answer = max(answer, values[i] + dp)
             dp = max(dp - 1, values[i] - 1)
-        return answer  
+        return answer
