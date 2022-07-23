7cdd84a7bd2ca770f879a36e94e9e7ae2393b132
Surya-827 <vsuryakiran827@gmail.com>
2022-07-23 21:30:36 +0530

    Uploaded Solution for Best time to buy II

---
+++ Competitive Codes/122_Best_Time_to_Buy_Sell_Stock_II.py
@@ -0,0 +1,24 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        cache = {}
+
+        def mxProf(i, st):
+            if i >= len(prices):
+                return 0
+
+            if (i, st) in cache:
+                return cache[(i, st)]
+
+            if st:
+                buy = mxProf(i + 1, not st) - prices[i]
+                cool = mxProf(i + 1, st)
+                res = max(buy, cool)
+            else:
+                sell = mxProf(i + 1, not st) + prices[i]
+                cool = mxProf(i + 1, st)
+                res = max(sell, cool)
+
+            cache[(i, st)] = res
+            return res
+
+        return mxProf(0, True)
