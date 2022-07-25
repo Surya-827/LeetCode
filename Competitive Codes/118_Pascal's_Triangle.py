dc106ab162a1a2a6fcf58ed1d1d56a195cd46ff2
Surya-827 <vsuryakiran827@gmail.com>
2022-07-25 10:26:45 +0530

    Modified Solution for Pascals Triangle

--- Competitive Codes/118_Pascal's_Triangle.py
+++ Competitive Codes/118_Pascal's_Triangle.py
@@ -34,6 +34,37 @@ class Solution:
             lst[rowIndex].append(temp[i] + temp[i + 1])
         return lst[rowIndex]
 
+#Way - 3
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        a = []
+        for x in range(numRows):
+            a.append([1])
+            # 			creating the 2d array
+            # 			appending the starting 1: [1, ...]
+            if x > 1:
+                # 			skipping the first two rows
+                for c in range(x - 1):
+                    # 					for loop used to find info about previous row
+                    a[x].append(a[x - 1][c] + a[x - 1][c + 1])
+            # 					math for adding the c and the c+1 number from the previous row
+            if x > 0:
+                # 			checking if its not the first row
+                a[x].append(1)
+        # 				appending the ending 1: [1, ..., 1]
+        return a
+
+# Version with no comments
+#         a = []
+#         for x in range(numRows):
+#             a.append([1])
+#             if x > 1:
+#                 for c in range(x-1):
+#                     a[x].append(a[x-1][c]+a[x-1][c+1])
+#             if x > 0:
+#                 a[x].append(1)
+#         return a
+
 if __name__=="__main__":
 
     n = int(input())
