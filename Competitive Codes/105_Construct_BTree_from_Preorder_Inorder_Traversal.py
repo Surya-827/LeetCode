57d6079420bd526809c0e10aac7d9f960f96a92f
Surya-827 <vsuryakiran827@gmail.com>
2022-07-29 14:08:07 +0530

    Uploaded SOLUTION for Preorder,Inorder Traversal

---
+++ Competitive Codes/105_Construct_BTree_from_Preorder_Inorder_Traversal.py
@@ -0,0 +1,27 @@
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+
+
+        # recursion in pre-order : conditions
+
+        # first value in pre-order traversal is root
+
+
+        # recursion in in-order : conditions
+
+        # every value to the left of the in-order root will go into left sub-tree
+
+        # every value to the right of the in-order root will go into rigth sub-tree
+
+        if not preorder or not inorder:
+            return None
+
+        root = TreeNode(preorder[0])
+
+        mid = inorder.index(preorder[0])
+
+        root.left = self.buildTree(preorder[1:mid+1], inorder[:mid])
+
+        root.right = self.buildTree(preorder[mid+1:], inorder[mid+1:])
+
+        return root
