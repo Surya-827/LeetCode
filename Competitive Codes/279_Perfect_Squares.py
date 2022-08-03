d2d94516710cb24ffe9d83c7a1f7b9636ad9d509
Surya Kiran <63420304+Surya-827@users.noreply.github.com>
2022-08-03 22:54:02 +0530

    updated solution for perfect squares

--- Competitive Codes/279_Perfect_Squares.py
+++ Competitive Codes/279_Perfect_Squares.py
@@ -1,12 +1,9 @@
+import math
 class Solution:
-    def uniquePaths(self, m: int, n: int) -> int:
-        '''one liner'''
-        return math.comb(m+n-2, m-1)
-
-if __name__=="__main__":
-
-    obj = Solution()
-    m,n = map(int,input().split())
-
-    print(obj.uniquePaths(m,n))
-
+    def numSquares(self, n: int) -> int:
+        dp = [float('inf') for _ in range(n+1)]
+        dp[0] = 0
+        for i in range(1, n+1):
+            for j in range(math.floor(math.sqrt(i)), 0, -1):
+                dp[i] = min(dp[i], 1 + dp[i-(j*j)])
+        return dp[-1]
